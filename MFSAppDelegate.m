fc4e077e4aadd4dfdb2c8f0e39b2a463b7fa14fd
kuyk15820-cpu <kuyk15820@gmail.com>
2026-08-14 00:06:32 +0700

    Update MFSAppDelegate.m

--- MFSAppDelegate.m
+++ MFSAppDelegate.m
@@ -5,7 +5,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	_window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-	_rootViewController = [[UINavigationController alloc] initWithRootViewController:[[MFSRootViewController alloc] init]];
+	
+	UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:[[MFSRootViewController alloc] init]];
+	if (@available(iOS 11.0, *)) {
+		navController.navigationBar.prefersLargeTitles = YES;
+	}
+	
+	_rootViewController = navController;
 	_window.rootViewController = _rootViewController;
 	[_window makeKeyAndVisible];
 	return YES;
